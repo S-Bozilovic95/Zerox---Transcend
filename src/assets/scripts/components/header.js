583465704687c1d7b281ed5cab9bc19db0894c6b
Lacedaimon95 <svetozar95@gmail.com>
2022-09-26 13:07:13 +0200

    master/ corrections

--- src/assets/scripts/components/header.js
+++ src/assets/scripts/components/header.js
@@ -1,29 +1,29 @@
-let hamburger = document.querySelector('.iconHamburger');
-let x = document.querySelector('.iconClose');
+let hamburger = document.querySelector('.icon-hamburger');
+let x = document.querySelector('.icon-close');
 let dropMenu = document.querySelector('.header__dropdown');
-let link = document.querySelectorAll('.header__dropdown__mainList__hasChild');
-let searchResponsive = document.querySelector('.iconSearch');
-let responsiveSearchBar = document.querySelector('.header__searchBar');
+let link = document.querySelectorAll('.header__dropdown__main-list__has-child');
+let searchResponsive = document.querySelector('.icon-search');
+let responsiveSearchBar = document.querySelector('.header__search-bar');
 let dropMenuOverlay = document.querySelector('.header__overlay2');
 let searchOverlay = document.querySelector('.header__overlay');
 
 // show dropdown menu
 if (hamburger && dropMenu && dropMenuOverlay) {
     hamburger.addEventListener('click', () => {
-        dropMenu.classList.add('dropdownShow');
-        dropMenuOverlay.classList.add('showOverlay');
+        dropMenu.classList.add('dropdown-show');
+        dropMenuOverlay.classList.add('show-overlay');
     });
 }
 
 // hide dropdown menu
 if (x && dropMenu && dropMenuOverlay) {
     x.addEventListener('click', () => {
-        dropMenu.classList.remove('dropdownShow');
-        dropMenuOverlay.classList.remove('showOverlay');
+        dropMenu.classList.remove('dropdown-show');
+        dropMenuOverlay.classList.remove('show-overlay');
     });
     dropMenuOverlay.addEventListener('click', () => {
-        dropMenu.classList.remove('dropdownShow');
-        dropMenuOverlay.classList.remove('showOverlay');
+        dropMenu.classList.remove('dropdown-show');
+        dropMenuOverlay.classList.remove('show-overlay');
     });
 }
 
@@ -31,14 +31,14 @@ if (x && dropMenu && dropMenuOverlay) {
 if (link) {
     link.forEach(el => {
         el.addEventListener('click', (e) => {
-            if (e.target.classList[0] === ('header__dropdown__mainList__hasChild') || e.target.classList[0] === ('header__dropdown__mainList__hasChild setActive')) {
-                e.target.classList.toggle('setActive');
-                e.target.firstChild.nextElementSibling.classList.toggle('chevRotate');
-                e.target.lastChild.previousElementSibling.classList.toggle('underListShow');
-            } else if (e.target.classList[0] === ('header__dropdown__mainList__hasChild__label')) {
-                e.target.parentElement.classList.toggle('setActive');
-                e.target.parentElement.firstChild.nextElementSibling.classList.toggle('chevRotate');
-                e.target.parentElement.lastChild.previousElementSibling.classList.toggle('underListShow');
+            if (e.target.classList[0] === ('header__dropdown__main-list__has-child') || e.target.classList[0] === ('header__dropdown__main-list__has-child set-active')) {
+                e.target.classList.toggle('set-ctive');
+                e.target.firstChild.nextElementSibling.classList.toggle('chev-rotate');
+                e.target.lastChild.previousElementSibling.classList.toggle('under-list-show');
+            } else if (e.target.classList[0] === ('header__dropdown__main-list__has-child__label')) {
+                e.target.parentElement.classList.toggle('set-active');
+                e.target.parentElement.firstChild.nextElementSibling.classList.toggle('chev-rotate');
+                e.target.parentElement.lastChild.previousElementSibling.classList.toggle('under-list-show');
             }
         });
     });
@@ -47,14 +47,14 @@ if (link) {
 // show and hide search bar in responsive screen
 if (searchResponsive && responsiveSearchBar) {
     searchResponsive.addEventListener('click', () => {
-        responsiveSearchBar.classList.add('showSearchBar');
-        searchOverlay.classList.add('showOverlay');
+        responsiveSearchBar.classList.add('show-search-bar');
+        searchOverlay.classList.add('show-overlay');
     });
 
     if (searchOverlay) {
         searchOverlay.addEventListener('click', () => {
-            responsiveSearchBar.classList.remove('showSearchBar');
-            searchOverlay.classList.remove('showOverlay');
+            responsiveSearchBar.classList.remove('show-search-bar');
+            searchOverlay.classList.remove('show-overlay');
         });
     }
 }
